bc07c110844b00c3dac8fc0570cecddb5ba7271b
Alex Mirapalheta <ctl_amirapalheta@uolinc.com>
2024-01-29 11:13:30 -0300

    develop refactor: exception handler extends and exceptions

---
+++ src/main/kotlin/com/kotlinspring/learn/userscrudapi/handler/ExceptionsHandler.kt
@@ -0,0 +1,115 @@
+package com.kotlinspring.learn.userscrudapi.handler
+
+import com.kotlinspring.learn.userscrudapi.exception.ExceptionResponse
+import com.kotlinspring.learn.userscrudapi.users.exception.UserNotFoundException
+import jakarta.servlet.http.HttpServletRequest
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import org.springframework.dao.DataIntegrityViolationException
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.http.converter.HttpMessageNotReadableException
+import org.springframework.web.bind.MethodArgumentNotValidException
+import org.springframework.web.bind.annotation.ControllerAdvice
+import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.context.request.WebRequest
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler
+import java.time.LocalDateTime
+
+@ControllerAdvice
+class ExceptionsHandler {
+
+    companion object {
+        private val logger: Logger = LoggerFactory.getLogger(ExceptionHandler::class.java)
+    }
+    @ExceptionHandler(Exception::class)
+    fun handleAllExceptions (ex: Exception, request: WebRequest): ResponseEntity<ExceptionResponse> {
+        val exceptionResponse = ExceptionResponse(
+            LocalDateTime.now(),
+            ex.message,
+            request.getDescription(false)
+        )
+
+        logger.info("handleAllExceptions [${HttpStatus.INTERNAL_SERVER_ERROR}] $ex")
+
+        return ResponseEntity<ExceptionResponse>(exceptionResponse, HttpStatus.INTERNAL_SERVER_ERROR)
+    }
+
+    @ExceptionHandler(UserNotFoundException::class)
+    fun handleUserNotFoundExceptions (ex: Exception, request: WebRequest): ResponseEntity<ExceptionResponse> {
+        val exceptionResponse = ExceptionResponse(
+            LocalDateTime.now(),
+            ex.message,
+            request.getDescription(false)
+        )
+
+        logger.info("handleUserNotFoundExceptions [${HttpStatus.NOT_FOUND}] $ex")
+
+        return ResponseEntity<ExceptionResponse>(exceptionResponse, HttpStatus.NOT_FOUND)
+    }
+
+    @ExceptionHandler(MethodArgumentTypeMismatchException::class)
+    fun handleValidationException (ex: MethodArgumentTypeMismatchException, request: WebRequest): ResponseEntity<ExceptionResponse> {
+        val exceptionResponse = ExceptionResponse(
+            LocalDateTime.now(),
+            ex.message,
+            request.getDescription(false)
+        )
+
+        logger.info("MethodArgumentTypeMismatchException [${HttpStatus.BAD_REQUEST}] $ex")
+
+        return ResponseEntity<ExceptionResponse>(exceptionResponse, HttpStatus.BAD_REQUEST)
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException::class)
+    fun handleMethodArguentNotValidException(
+        req: HttpServletRequest,
+        ex: MethodArgumentNotValidException
+    ): ResponseEntity<ExceptionResponse> {
+        val exceptionResponse = ExceptionResponse(
+                LocalDateTime.now(),
+                "Invalid Payload",
+                (ex.bindingResult.allErrors.map { it.defaultMessage ?: "" }).reduce { acc, s -> "$acc; $s" }
+        )
+
+        logger.info("handleMethodArguentNotValidException [${HttpStatus.BAD_REQUEST}] $ex")
+
+        return ResponseEntity<ExceptionResponse>(exceptionResponse, HttpStatus.BAD_REQUEST)
+    }
+
+    @ExceptionHandler(DataIntegrityViolationException::class)
+    fun handleDataIntegrityViolationException(
+        ex: DataIntegrityViolationException,
+        request: WebRequest
+    ): ResponseEntity<ExceptionResponse>
+    {
+        val exceptionResponse = ExceptionResponse(
+            LocalDateTime.now(),
+            "Exclusive Data Restriction",
+            "Could Not Execute Statement. Check Provided Payload"
+        )
+
+        logger.info("handleDataIntegrityViolationException [${HttpStatus.BAD_REQUEST}] $ex")
+
+        return ResponseEntity<ExceptionResponse>(exceptionResponse, HttpStatus.BAD_REQUEST)
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException::class)
+    fun handleHttpMessageNotReadableException(
+        ex: HttpMessageNotReadableException,
+        request: WebRequest
+    ): ResponseEntity<ExceptionResponse>
+    {
+        val exceptionResponse = ExceptionResponse(
+            LocalDateTime.now(),
+            "Http Message Not Readable",
+            ex.message ?: ""
+        )
+
+        logger.info("HttpMessageNotReadableException [${HttpStatus.BAD_REQUEST}] $ex")
+
+        return ResponseEntity<ExceptionResponse>(exceptionResponse, HttpStatus.BAD_REQUEST)
+    }
+
+}
